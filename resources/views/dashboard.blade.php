45f5e0117e5b838bd3f00c62d66a018899fab81d
Johnny Saymon <github@saymon.dev>
2022-05-13 19:17:05 -0300

    Adição do campo para cadastro de URLs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <form action="/url-check" method="POST">
+                        <x-label value="URL" />
+                        <x-input name="url"/>
+                        <x-button>Adicionar</x-button>
+                    </form>
                 </div>
             </div>
         </div>
